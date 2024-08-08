45c4d48ccd3a8aa161862af672ae26872b298204
JoshuaMooneyham <jmooneyham58@gmail.com>
2024-08-08 00:05:18 -0500

    railway test

--- auction/models.py
+++ auction/models.py
@@ -6,6 +6,9 @@ class Bidder(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "bidder_info")
     bidder_id = models.IntegerField(null=True, blank=True)
 
+class ItemImages(models.Model):
+    file = models.ImageField(upload_to="", blank=True, null=True)
+
 class AuctionItem(models.Model):
     name = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -15,6 +18,8 @@ class AuctionItem(models.Model):
     highest_bidder = models.ForeignKey(Bidder, on_delete=models.CASCADE)
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
+    images = models.ForeignKey(ItemImages, on_delete=models.SET_NULL)
+
     
 class Bid(models.Model):
     bidder = models.ForeignKey(Bidder, on_delete=models.CASCADE)
